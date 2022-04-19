03c4ac47030784a7648012fe8fa63f9807ebf8b0
siddharthsingh025 <siddharthsingh25102001@gmail.com>
2022-04-19 12:31:39 +0530

    new feature in progress (#1)

---
+++ app/src/main/java/com/internshala/foodrunner/activity/SearchBar_activity.kt
@@ -0,0 +1,15 @@
+package com.internshala.foodrunner.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.internshala.foodrunner.databinding.ActivityDashboardBinding
+
+class SearchBar_activity : AppCompatActivity() {
+
+    lateinit var binding : ActivityDashboardBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
